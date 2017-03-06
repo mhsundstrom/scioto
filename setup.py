1421c19495d2d10a9d2c8e5151f88044e3ee58bf
Mark Sundstrom <mark@mhsundstrom.com>
2017-03-06 04:39:13 -0500

    Added the templated decorator, something else I use frequently.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='scioto',
-    version='2017.221.36',
+    version='2017.306.0',
     description='Miscellaneous useful functions',
     author='Mark Sundstrom',
     author_email='mark@mhsundstrom.com',
