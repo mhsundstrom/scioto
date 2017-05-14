e5ee6d66e1360fb1cad17d797a5c403ecc7a1107
Mark Sundstrom <mark@mhsundstrom.com>
2017-05-14 15:47:33 -0400

    wip generalized get_altitude into 'observe'

--- scioto/sky.py
+++ scioto/sky.py
@@ -13,9 +13,12 @@ TWILIGHT = ('Dawn', 'Dusk')
 HOUR = 1 / 24
 
 
+# TODO add the Loader as an optional argument at the end. If it's None,
+# then do the loading. OR: pass in planets and timescale (ts) and if
+# either are None then have them loaded.
 def generate_horizon_events(
         earth_location: tuple,
-        body_name: str,
+        body_name: str,  # TODO could also be a tuple of names?
         starting_time=None,
         days: int=1,
         kind: tuple=RISE_SET,
@@ -34,27 +37,28 @@ def generate_horizon_events(
     hours = 24 * days + 1
     jd = ts.tt(jd=np.linspace(tt, tt + days, hours))
 
-    calculated_azimuth = None  # Storage for the azimuth we wish to return
-
-    def get_altitude(time):
-        # For use with brentq below.
-        nonlocal calculated_azimuth  # Save the azimuth in the outer level.
-
-        if not isinstance(time, Time):
-            time = ts.tt(jd=time)
-        alt, calculated_azimuth, _ = where.at(time).observe(body).apparent().altaz('standard')
-        return alt._degrees - altitude
-
     alt, *_ = where.at(jd).observe(body).apparent().altaz('standard')
 
     # Is the next event a rising or a setting.
     current_altitude = alt._degrees[0]
     which_kind = 1 if current_altitude > altitude else 0
     change_dates = jd.tt[np.ediff1d(np.sign(alt._degrees - altitude), to_end=0) != 0]
+    args = (where, body, ts, 'alt', altitude)
     for date in change_dates:
-        t = ts.tt(jd=brentq(get_altitude, date, date + HOUR))
-        yield (t, body_name, kind[which_kind], calculated_azimuth)
+        # t = ts.tt(jd=brentq(get_altitude, date, date + HOUR))
+        t = ts.tt(jd=brentq(observe, date, date + HOUR, args=args))
+        alt, az, _ = where.at(jd=t).observe(body).apparent().altaz('standard')
+        yield (t, body_name, kind[which_kind], az)
         which_kind = 1 - which_kind
+
+
+def observe(time, where, body, ts, field, offset):
+    if not isinstance(time, Time):
+        time = ts.tt(jd=time)
+        alt, az, _ = where.at(time).observe(body).apparent().altaz('standard')
+        value = alt if field == 'alt' else az
+        return value._degrees - offset
+
 
 
 def upcoming_rise_and_set_events():
@@ -70,7 +74,7 @@ def upcoming_rise_and_set_events():
         t = dt.Pendulum.instance(t.utc_datetime()).in_tz('local')
         print(f"{name.split()[0].title():8} {kind:4} {az} {t:%H:%M %a %-d %b}")
 
-# upcoming_rise_and_set_events()
+upcoming_rise_and_set_events()
 
 
 def calculate_azimuth_event(
@@ -112,9 +116,9 @@ def calculate_azimuth_event(
     raise ValueError(f"Can't find azimuth={azimuth}")
 
 
-where = ('40 N', '83 W')
-azimuth = 180
-for name in ('sun', 'moon', 'mercury', 'venus', 'mars'):
-    t, alt = calculate_azimuth_event(where, name, azimuth=azimuth)
-    t = dt.Pendulum.instance(t.utc_datetime()).in_tz('local')
-    print(f"{name.title():8} {t:%H:%M %a %-d %b} At {azimuth}: {alt}")
+# where = ('40 N', '83 W')
+# azimuth = 180
+# for name in ('sun', 'moon', 'mercury', 'venus', 'mars'):
+#     t, alt = calculate_azimuth_event(where, name, azimuth=azimuth)
+#     t = dt.Pendulum.instance(t.utc_datetime()).in_tz('local')
+#     print(f"{name.title():8} {t:%H:%M %a %-d %b} At {azimuth}: {alt}")
