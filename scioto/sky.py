7daed5df65748d12a67bb369c595366441b042ea
Mark Sundstrom <mark@mhsundstrom.com>
2017-05-14 15:56:39 -0400

    wip generate_horizon_events now handles a tuple of names as well as a str.

--- scioto/sky.py
+++ scioto/sky.py
@@ -13,12 +13,9 @@ TWILIGHT = ('Dawn', 'Dusk')
 HOUR = 1 / 24
 
 
-# TODO add the Loader as an optional argument at the end. If it's None,
-# then do the loading. OR: pass in planets and timescale (ts) and if
-# either are None then have them loaded.
 def generate_horizon_events(
         earth_location: tuple,
-        body_name: str,  # TODO could also be a tuple of names?
+        names,  # Either a str or a tuple of str
         starting_time=None,
         days: int=1,
         kind: tuple=RISE_SET,
@@ -29,7 +26,6 @@ def generate_horizon_events(
     planets = load(SPICE_KERNEL)
     earth = planets['earth']
     where = earth + Topos(*earth_location)
-    body = planets[body_name]
     ts = load.timescale()
 
     # Times every hour for the requested days
@@ -37,39 +33,45 @@ def generate_horizon_events(
     hours = 24 * days + 1
     jd = ts.tt(jd=np.linspace(tt, tt + days, hours))
 
-    alt, *_ = where.at(jd).observe(body).apparent().altaz('standard')
+    if isinstance(names, str):
+        names = (names,)
 
-    # Is the next event a rising or a setting.
-    current_altitude = alt._degrees[0]
-    which_kind = 1 if current_altitude > altitude else 0
-    change_dates = jd.tt[np.ediff1d(np.sign(alt._degrees - altitude), to_end=0) != 0]
-    args = (where, body, ts, 'alt', altitude)
-    for date in change_dates:
-        # t = ts.tt(jd=brentq(get_altitude, date, date + HOUR))
-        t = ts.tt(jd=brentq(observe, date, date + HOUR, args=args))
-        alt, az, _ = where.at(jd=t).observe(body).apparent().altaz('standard')
-        yield (t, body_name, kind[which_kind], az)
-        which_kind = 1 - which_kind
+    for body_name in names:
+        body = planets[body_name]
+
+        alt, *_ = where.at(jd).observe(body).apparent().altaz('standard')
+
+        # Is the next event a rising or a setting.
+        current_altitude = alt._degrees[0]
+        which_kind = 1 if current_altitude > altitude else 0
+        change_dates = jd.tt[np.ediff1d(np.sign(alt._degrees - altitude), to_end=0) != 0]
+        args = (where, body, ts, 'alt', altitude)
+        for date in change_dates:
+            # t = ts.tt(jd=brentq(get_altitude, date, date + HOUR))
+            t = ts.tt(jd=brentq(observe, date, date + HOUR, args=args))
+            alt, az, _ = where.at(jd=t).observe(body).apparent().altaz('standard')
+            yield (t, body_name, kind[which_kind], az)
+            which_kind = 1 - which_kind
 
 
 def observe(time, where, body, ts, field, offset):
     if not isinstance(time, Time):
         time = ts.tt(jd=time)
-        alt, az, _ = where.at(time).observe(body).apparent().altaz('standard')
-        value = alt if field == 'alt' else az
-        return value._degrees - offset
+    alt, az, _ = where.at(time).observe(body).apparent().altaz('standard')
+    value = alt if field == 'alt' else az
+    return value._degrees - offset
 
 
 
 def upcoming_rise_and_set_events():
     events = []
     where = ('40 N', '83 W')
-    for name in ('sun', 'moon', 'venus', 'jupiter barycenter', 'mars', 'saturn barycenter', 'mercury'):
-        for event in generate_horizon_events(where, name, days=1):
-            events.append(event)
-        if name == 'sun':
-            for event in generate_horizon_events(where, name, days=1, kind=TWILIGHT, altitude=-6):
-                events.append(event)
+    names = ('sun', 'moon', 'mercury', 'venus', 'mars',
+             'jupiter barycenter', 'saturn barycenter')
+    for event in generate_horizon_events(where, names, days=1):
+        events.append(event)
+    for event in generate_horizon_events(where, 'sun', days=1, kind=TWILIGHT, altitude=-6):
+        events.append(event)
     for t, name, kind, az in sorted(events, key=lambda x: x[0].tt):
         t = dt.Pendulum.instance(t.utc_datetime()).in_tz('local')
         print(f"{name.split()[0].title():8} {kind:4} {az} {t:%H:%M %a %-d %b}")
